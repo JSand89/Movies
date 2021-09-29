3d7e854e043bfc4a13ceb81099ef12320e8aa2cb
JSand89 <javier.esand@gmail.com>
2021-09-29 10:06:01 -0500

    adelanto cliente

--- client/src/app/components/movie-form/movie-form.component.ts
+++ client/src/app/components/movie-form/movie-form.component.ts
@@ -1,11 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, HostBinding, OnInit } from '@angular/core';
 @Component({
   selector: 'app-movie-form',
   templateUrl: './movie-form.component.html',
   styleUrls: ['./movie-form.component.css']
 })
 export class MovieFormComponent implements OnInit {
+
+  @HostBinding('class') calsses='row'
+
+  movie:any={
+      id:0,
+      title:'',
+      year: 0,
+      time: 0,
+      lang: '',
+      relatedate:'string',
+      country: 'string',
+  }
 
   constructor() { }
 
