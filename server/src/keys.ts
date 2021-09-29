825127b4cbb403ef0b9a5670c5e40f9d065e457a
JSand89 <javier.esand@gmail.com>
2021-09-28 23:13:09 -0500

    Conectada bd alojada en clever cloud

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,12 +1,10 @@
 
 export default{
     database:{
-        host:'localhost',
-        user:'root',
-        password:'password',
-        database:'mydbmovies',
-        insecureAuth : true,
+        host:'blo2jl0ylaycfqydjzt9-mysql.services.clever-cloud.com',
+        user:'ucy59pnolacosd0x',
+        password:'m7MxAbpDTz17q77GL0uY',
+        database:'blo2jl0ylaycfqydjzt9',
         port:3306
-
     }
 }
